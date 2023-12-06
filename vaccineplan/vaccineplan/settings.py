a9b60019ef7b66229d8c91de3dbc4a0bfc813799
xtern0o <dos30028gmail.com>
2023-12-06 20:36:41 +0300

    Initial commit

---
+++ vaccineplan/vaccineplan/settings.py
@@ -0,0 +1,165 @@
+import os
+import pathlib
+
+from django.utils.translation import gettext_lazy as _
+from dotenv.main import load_dotenv
+
+__all__ = []
+
+load_dotenv()
+
+
+def load_bool_env(name, default):
+    env_value = os.getenv(name, str(default)).lower()
+    return env_value in ("true", "1", "yes", "y", "t")
+
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "NO_KEY")
+
+DEBUG = load_bool_env("DJANGO_DEBUG", False)
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
+INSTALLED_APPS = [
+    "homepage.apps.HomepageConfig",
+    "core.apps.CoreConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "sorl.thumbnail",
+    "django_cleanup.apps.CleanupConfig",
+    "widget_tweaks",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+
+ROOT_URLCONF = "vaccineplan.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "vaccineplan.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
+    },
+]
+
+LOGIN_URL = "/auth/login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/auth/login"
+
+if DEBUG:
+    DEFAULT_USER_IS_ACTIVE = True
+else:
+    DEFAULT_USER_IS_ACTIVE = load_bool_env("DEFAULT_USER_IS_ACTIVE", False)
+
+LANGUAGE_CODE = "ru"
+LANGUAGES = [
+    ("ru", _("Russian")),
+    ("en", _("English")),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static_dev",
+]
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CKEDITOR_CONFIGS = {
+    "default": {
+        "height": 300,
+        "width": 600,
+        "toolbar": "Custom",
+        "toolbar_Custom": [
+            [
+                "Undo",
+                "Redo",
+                "-",
+                "Font",
+                "FontSize",
+                "-",
+                "Bold",
+                "Italic",
+                "Underline",
+            ],
+            [
+                "JustifyLeft",
+                "JustifyCenter",
+                "JustifyRight",
+                "JustifyBlock",
+            ],
+        ],
+    },
+}
---
+++ vaccineplan/vaccineplan/urls.py
@@ -0,0 +1,29 @@
+import django.conf
+import django.conf.urls.static
+import django.contrib.admin
+import django.urls
+
+
+urlpatterns = [
+    django.urls.path("admin/", django.contrib.admin.site.urls),
+    django.urls.path("", django.urls.include("homepage.urls")),
+]
+
+urlpatterns += django.conf.urls.static.static(
+    django.conf.settings.STATIC_URL,
+    document_root=django.conf.settings.STATIC_ROOT,
+)
+urlpatterns += django.conf.urls.static.static(
+    django.conf.settings.MEDIA_URL,
+    document_root=django.conf.settings.MEDIA_ROOT,
+)
+
+if django.conf.settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (
+        django.urls.path(
+            "__debug__/",
+            django.urls.include(debug_toolbar.urls),
+        ),
+    )
