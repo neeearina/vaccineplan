9ca53919f4c7c220c84151a64b322f1c87ff158a
neeearina <arishschkarussia@gmail.com>
2023-12-07 14:06:01 +0500

    parser for categories

--- vaccineplan/vaccines/models.py
+++ vaccineplan/vaccines/models.py
@@ -12,13 +12,23 @@ class VaccineCategories(models.Model):
         verbose_name="болезнь",
     )
     description = models.TextField(
+        blank=True,
         help_text="описание болезни",
         verbose_name="описание",
     )
     num_of_vaccines = models.PositiveIntegerField(
+        default=0,
+        blank=True,
         help_text="общее количество разработанных вакцин от этой болезни",
         verbose_name="количество вакцин",
     )
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return f"Категория вакцины {self.name}"
 
 
 class Vaccines(models.Model):
@@ -32,6 +42,7 @@ class Vaccines(models.Model):
     category = models.ForeignKey(
         VaccineCategories,
         on_delete=models.CASCADE,
+        null=True,
         help_text="категория вакцины - название болезни,"
         " от которой она предназначена",
         verbose_name="категория",
