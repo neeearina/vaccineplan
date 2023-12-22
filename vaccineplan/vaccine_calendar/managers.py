75a16e7daacf8112787555dd5bf1c00d18b9898f
Aramarchuk <aramarchuk42@gmail.com>
2023-12-22 19:42:09 +0500

    [fix] black

--- vaccineplan/vaccine_calendar/managers.py
+++ vaccineplan/vaccine_calendar/managers.py
@@ -3,14 +3,19 @@ import django.db.models as models
 
 class VaccineScheduleManager(models.Manager):
     def get_user_records(self, user):
-        return self.get_queryset().filter(
-            user=user,
-        ).select_related(
-            "clinic",
-            "vaccine",
-        ).only(
-            "clinic__address",
-            "clinic__phone_number",
-            "vaccine__name",
-            "timetable",
+        return (
+            self.get_queryset()
+            .filter(
+                user=user,
+            )
+            .select_related(
+                "clinic",
+                "vaccine",
+            )
+            .only(
+                "clinic__address",
+                "clinic__phone_number",
+                "vaccine__name",
+                "timetable",
+            )
         )
--- vaccineplan/vaccines/managers.py
+++ vaccineplan/vaccines/managers.py
@@ -3,13 +3,22 @@ import django.db.models
 
 class AvaliabilityManager(django.db.models.Manager):
     def get_by_illness(self, illness):
-        return self.get_queryset().filter(
-            vaccines__category=illness,
-        ).filter(
-            clinic__approved=True,
+        return (
+            self.get_queryset()
+            .filter(
+                vaccines__category=illness,
+            )
+            .filter(
+                clinic__approved=True,
+            )
         )
 
     def get_already_checked(self, clinic):
-        return self.get_queryset().filter(
-            clinic=clinic,
-        ).select_related("vaccines").only("vaccines__id")
+        return (
+            self.get_queryset()
+            .filter(
+                clinic=clinic,
+            )
+            .select_related("vaccines")
+            .only("vaccines__id")
+        )
